4e69cdcebbdcc7b7df48442251ef96c128664533
SayuryKato Kato <skdp.ecp19@uea.edu.br>
2023-08-31 01:35:42 +0000

    :tada: Primeiro Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask('meu app')
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+@app.route('/novo')
+def novo():
+    return '<h1> Nova página </h1>'
