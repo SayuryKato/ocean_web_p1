ff3b2b161d9af87a57b1368f4509c40aea3c836a
SayuryKato Kato <skdp.ecp19@uea.edu.br>
2023-09-05 01:09:15 +0000

    :tada: rota e HTML da página principal

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask('meu app')
 
+post = [
+    {
+        "titulo": "Minha primeira postagem",
+        "texto": "teste"
+    },
+    {
+        "titulo": "Segundo post",
+        "texto": "Outro teste"
+    }
+]
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+def exibir_entradas():
+    entradas = post #Moc das postagens
+    return render_template('exibir_entradas.html', entradas=entradas)
 
 @app.route('/novo')
 def novo():
